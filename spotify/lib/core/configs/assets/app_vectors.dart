34620d3ea2f6ade1894a765d53d723a0d12da548
bEinG_RudY <pawansinghfea3@gmail.com>
2026-01-30 23:58:26 +0530

    splash page

---
+++ spotify/lib/core/configs/assets/app_vectors.dart
@@ -0,0 +1,7 @@
+class AppVectors {
+  static const String basePath = 'vectors/';
+  static const String svgFormat = '.svg';
+  static const String pngFormat = '.png';
+
+  static const String logo = '${basePath}Spotify-Logo$pngFormat';
+}
