0deb4e2113c053ad033140a3eab80649c84b78da
bEinG_RudY <pawansinghfea3@gmail.com>
2026-02-08 23:14:36 +0530

    gradle issue is in effect, working on it

---
+++ spotify/lib/presentation/auth/pages/signup_or_signin.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotify/core/configs/assets/app_vectors.dart';
+
+class SignupOrSigninPage extends StatelessWidget {
+  const SignupOrSigninPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Align(
+            alignment: Alignment.topRight,
+            child: SvgPicture.asset(AppVectors.topPattern),
+          )
+        ],
+      ),
+    );
+  }
+}
