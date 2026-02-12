c89cfeeb94049254bdc8269ff3527ac7ef8ab942
bEinG_RudY <pawansinghfea3@gmail.com>
2026-02-12 23:23:14 +0530

    sign in/sign up page in progress

--- spotify/lib/presentation/auth/pages/signup_or_signin.dart
+++ spotify/lib/presentation/auth/pages/signup_or_signin.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotify/core/configs/assets/app_images.dart';
 import 'package:spotify/core/configs/assets/app_vectors.dart';
 
 class SignupOrSigninPage extends StatelessWidget {
@@ -13,7 +14,27 @@ class SignupOrSigninPage extends StatelessWidget {
           Align(
             alignment: Alignment.topRight,
             child: SvgPicture.asset(AppVectors.topPattern),
-          )
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: SvgPicture.asset(AppVectors.botttomPattern),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Image.asset(AppImages.authBG),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Column(
+              children: [
+                SvgPicture.asset(AppVectors.logo),
+                Text(
+                  "Enjoy listening to music",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
+                )
+              ],
+            ),
+          ),
         ],
       ),
     );
